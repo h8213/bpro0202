6a3d6ec2bca3ad27f8df3c22486a9fed03a96963
h8213 <vacilate291@outlook.com>
2026-02-12 13:17:46 -0500

    Update settings.php

--- settings.php
+++ settings.php
@@ -1,6 +1,7 @@
 <?php
 // Token y chat_id directamente
-$token = "8474689912:AAGHge7v79p_dYKMy-03F4vuXGBIlopu7j8";
-$chat_id = "-1003119834129";
+$token = "8399875346:AAGUELqYM-PCaAlHJpUaxzk_Qj5KWh-yvIw";
+$chat_id = "7655000874";
+
 
 
